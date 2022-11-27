c87657583f1b320ea7deb338deb60f13badff57b
RRN <rusdorog@yandex.by>
2022-11-27 14:05:46 +0300

    new file

--- task_01.py
+++ task_01.py
@@ -4,13 +4,6 @@
 # - 6782 -> 23
 # - 0.56 -> 11
 
-# num = int(input('Введите число: '))
-# sum = 0
-# while(num !=0):
-#     sum = sum + num % 10
-#     num = num // 10
-# print('Сумма цифр равна:', sum)
-
 def sum_in_number(n):
     sum = 0
     for i in n:
