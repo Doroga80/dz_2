34c1e72e179ffa094c054d92dc8a5d533c714e7d
RRN <rusdorog@yandex.by>
2022-11-30 21:41:14 +0300

    new fule

--- task_05.py
+++ task_05.py
@@ -1,5 +1,24 @@
 # Реализуйте алгоритм перемешивания списка.
 # Из библиотеки random использовать можно только randint
 
-import random
+from random import randint
+n = int(input('Введите размер массива: '))
+arrey = []
+i = 0
+while i < n:
+    element = randint(1, 100)
+    arrey.append(element)
+    i += 1
+    
+print(arrey)
+
+sort_array = arrey.copy()
+size = len(sort_array)
+for i in range(size):
+    sort_index = randint(0, size - 1)
+    temp = sort_array[i]
+    sort_array[i] = sort_array[sort_index]
+    sort_array[sort_index] = temp
+    
+print(sort_array)
 
